17f4186d356c102b1509c3854096af67c43863a4
osvalds <osvalds.neiders@gmail.com>
2020-04-15 09:56:38 +0300

    Create the correct groupings for ship selector

--- src/components/ShipSelector.js
+++ src/components/ShipSelector.js
@@ -20,9 +20,7 @@ export const shipSize3 = [
     {
         mutations: 1,
         template: [
-            [1],
-            [1],
-            [1]
+            [1, 1, 1]
         ]
     },
     {
@@ -83,6 +81,84 @@ export const shipSize4 = [
     }
 ];
 
+const ship1 = {
+    mutations: 0,
+    template: [[1]]
+}
+
+const ship2 = {
+    mutations: 1,
+    template: [[1, 1]]
+}
+
+const ship3Straight = {
+    mutations: 1,
+    template: [
+        [1, 1, 1]
+    ]
+};
+
+const ship3L = {
+    mutations: 3,
+    template: [
+        [1, 1],
+        [1, 0]
+    ]
+};
+
+const ship4Block = {
+    mutations: 0,
+    template: [
+        [1, 1],
+        [1, 1],
+    ]
+};
+
+const ship4Straight = {
+    mutations: 1,
+    template: [[1, 1, 1, 1]]
+}
+
+const ship4L = {
+    mutations: 3,
+    template: [
+        [1, 1, 1],
+        [1, 0, 0]
+    ]
+}
+
+const ship4LFlipped = {
+    mutations: 3,
+    template: [
+        [1, 0, 0],
+        [1, 1, 1]
+    ]
+}
+
+const ship4T = {
+    mutations: 3,
+    template: [
+        [0, 1, 0],
+        [1, 1, 1]
+    ]
+}
+
+const ship4Z = {
+    mutations: 3,
+    template: [
+        [1, 1, 0],
+        [0, 1, 1]
+    ]
+}
+
+const ship4ZFlipped = {
+    mutations: 3,
+    template: [
+        [0, 1, 1],
+        [1, 1, 0]
+    ]
+}
+
 export const rotateTemplate90deg = (template) => {
     const {rows, cols} = getDimensions(template);
 
@@ -97,14 +173,16 @@ export const rotateTemplate90deg = (template) => {
     return rotatedTemplate;
 };
 
+const getAllRotations = ({template, mutations}) => {
+    let rotations = [template]
+    for (let i = 0; i < mutations; i++) {
+        rotations.push(rotateTemplate90deg(rotations[i]))
+    }
+    return rotations
+}
+
 export const createAllShipPermutations = (shipTemplates) => {
-    return shipTemplates.map(({template, mutations}) => {
-        let rotations = [template]
-        for (let i = 0; i < mutations; i++) {
-            rotations.push(rotateTemplate90deg(rotations[i]))
-        }
-        return rotations
-    })
+    return shipTemplates.map(getAllRotations)
 };
 
 export const allShipPermutationsFlat = (shipTemplates) => {
@@ -122,35 +200,68 @@ const ShipTemplateRow = React.memo(({templates, setDraggedShip, templateName}) =
 })
 
 export const ShipSelector = React.memo(({setDraggedShip}) => {
-        const allShip1 = createAllShipPermutations(shipSize1);
-        const allShip2 = createAllShipPermutations(shipSize2);
-        const allShip3 = createAllShipPermutations(shipSize3);
-        const allShip4 = createAllShipPermutations(shipSize4);
+        const ship1Rotated = getAllRotations(ship1);
+
+        const ship2Rotated = getAllRotations(ship2);
+
+        const ship3StraightRotated = getAllRotations(ship3Straight);
+        const ship3LRotated = getAllRotations(ship3L);
+
+        const ship4BlockRotated = getAllRotations(ship4Block);
+        const ship4StraightRotated = getAllRotations(ship4Straight);
+        const ship4LRotated = getAllRotations(ship4L).concat(getAllRotations(ship4LFlipped));
+        const ship4TRotated = getAllRotations(ship4T);
+        const ship4ZRotated = getAllRotations(ship4Z).concat(getAllRotations(ship4ZFlipped));;
+
         return (
-            // null
             <div className="ship-selector">
-                {allShip1.map((templates, i) => <ShipTemplateRow key={`allship1-${i}`}
-                                                                 setDraggedShip={setDraggedShip}
-                                                                 templateName="allship1"
-                                                                 templates={templates}/>)}
+                <div>
+                    <ShipTemplateRow key={`ship1`}
+                                     setDraggedShip={setDraggedShip}
+                                     templateName="ship1"
+                                     templates={ship1Rotated}/>
+                </div>
+                <div>
+                    <ShipTemplateRow key={`ship2`}
+                                     setDraggedShip={setDraggedShip}
+                                     templateName="ship2"
+                                     templates={ship2Rotated}/>
+                </div>
+                <div>
+                    <ShipTemplateRow key={`allship3-straight`}
+                                     setDraggedShip={setDraggedShip}
+                                     templateName="ship3-straight"
+                                     templates={ship3StraightRotated}/>
 
-                {allShip2.map((templates, i) => <ShipTemplateRow key={`allship2-${i}`}
-                                                                 setDraggedShip={setDraggedShip}
-                                                                 templateName="allship2"
-                                                                 templates={templates}/>)}
+                    <ShipTemplateRow key={`allship3-l`}
+                                     setDraggedShip={setDraggedShip}
+                                     templateName="ship3-l"
+                                     templates={ship3LRotated}/>
+                </div>
+                <div>
+                    <ShipTemplateRow key={`allship4-straight`}
+                                     setDraggedShip={setDraggedShip}
+                                     templateName="ship4-straight"
+                                     templates={ship4StraightRotated}/>
+                    <ShipTemplateRow key={`allship4-block`}
+                                     setDraggedShip={setDraggedShip}
+                                     templateName="ship4-block"
+                                     templates={ship4BlockRotated}/>
+                    <ShipTemplateRow key={`allship4-t`}
+                                     setDraggedShip={setDraggedShip}
+                                     templateName="ship4-t"
+                                     templates={ship4TRotated}/>
+                    <ShipTemplateRow key={`allship4-z`}
+                                     setDraggedShip={setDraggedShip}
+                                     templateName="ship4-z"
+                                     templates={ship4ZRotated}/>
+                    <ShipTemplateRow key={`allship4-l`}
+                                     setDraggedShip={setDraggedShip}
+                                     templateName="ship4-l"
+                                     templates={ship4LRotated}/>
 
-                {allShip3.map((templates, i) => <ShipTemplateRow key={`allship3-${i}`}
-                                                                 setDraggedShip={setDraggedShip}
-                                                                 templateName="allship3"
-                                                                 templates={templates}/>)}
-
-                {allShip4.map((templates, i) => <ShipTemplateRow key={`allship4-${i}`}
-                                                                 templates={templates}
-                                                                 templateName="allship4"
-                                                                 setDraggedShip={setDraggedShip}/>)}
-
+                </div>
             </div>
-
         )
 
     }
